c8a9f42905f9a5fb4e6f76cf300a7d9563dbb11e
Bryce Hipp <stiffhipp@gmail.com>
2020-05-30 01:22:32 -0500

    Correct paths to file for purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,9 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
     './index.html',
-    './App.vue',
+    './src/App.vue',
     './components/**/*.vue',
-    './views/**/*.vue',
-    './main.js',
-    './src/**/*.js',
-    // etc.
+    './src/main.js',
   ],
 
   // Include any special characters you're using in this regular expression
